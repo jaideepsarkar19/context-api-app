a6cecd5e89c52c75a545c62107b43d2482e88de0
jaideepsarkar19 <jaideep.sarkar19@rediffmail.com>
2023-05-09 15:07:38 +0530

    1st

---
+++ src/components/studentdetails.js
@@ -0,0 +1,83 @@
+const StudentDetails = [
+{   
+    id    :1,
+    name  :"kalyan",
+    age   :17,
+    gender:"male",
+    class :"11",
+    who   :'student'
+},
+{   
+    id    :2,
+    name  :"rajib",
+    age   :19,
+    gender:"male",
+    class :"12",
+    who   :'student'
+},
+{   
+    id    :3,
+    name  :"rakesh",
+    age   :14,
+    gender:"female",
+    class :"10",
+    who   :'student'
+},
+{   
+    id    :4,
+    name  :"rachana",
+    age   :15,
+    gender:"female",
+    class :"11",
+    who   :'student'
+},
+{   
+    id    :5,
+    name  :"sarita",
+    age   :18,
+    gender:"female",
+    class :"12",
+    who   :'student'
+},
+{   
+    id    :6,
+    name  :"sonam",
+    age   :18,
+    gender:"female",
+    class :"12",
+    who   :'student'
+},
+{   
+    id    :7,
+    name  :"nisha",
+    age   :19,
+    gender:"female",
+    class :"12",
+    who   :'student'
+},
+{   
+    id    :8,
+    name  :"amrita",
+    age   :19,
+    gender:"female",
+    class :"12",
+    who   :'student'
+},
+{   
+    id    :9,
+    name  :"lisa",
+    age   :17,
+    gender:"female",
+    class :"11",
+    who   :'student'
+},
+{   
+    id    :10,
+    name  :"navneeta",
+    age   :15,
+    gender:"female",
+    class :"10",
+    who   :'student'
+},
+]
+export default StudentDetails ;
---
+++ src/components/teachersdetails.js
@@ -0,0 +1,28 @@
+const teachersData=[{
+    id   :"1",
+    name : "Pradip",
+    age  : 49,
+    gender:'male',
+    email: "pradip@abc.com",
+    class: 12,
+    who  : 'teahcer'
+},{
+    id   : "2",
+    name : "suraj",
+    age  : 52,
+    gender:'male',
+    email: "suraj@gmail.com",
+    class: 10,
+    who  : 'teahcer'
+},{
+    id   :"3",
+    name : "Simran",
+    age  : 40,
+    gender:'female',
+    email: "ssimran@mail.com",
+    class: 11,
+    who  : 'teahcer'
+}
+]
+
+export default teachersData ;
